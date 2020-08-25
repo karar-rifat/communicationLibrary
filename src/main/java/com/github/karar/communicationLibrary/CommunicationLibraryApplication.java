f07a96f12dfe754f485564da26af3e2ba2a68ecd
karar rifat <kararrifat@gmail.com>
2020-08-25 14:13:36 +0600

    first commit

---
+++ src/main/java/com/github/karar/communicationLibrary/CommunicationLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.github.karar.communicationLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationLibraryApplication.class, args);
+	}
+
+}
